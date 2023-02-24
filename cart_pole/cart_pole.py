428204a5d6e0c666847a757319980bd5c39a5b6a
Prof. Sandeco <sandecom@gmail.com>
2023-02-24 08:17:37 -0300

    Cart Pole

--- cart_pole/cart_pole.py
+++ cart_pole/cart_pole.py
@@ -1,5 +1,6 @@
 import gym
 import numpy as np
+import pickle
 
 env = gym.make('CartPole-v1')
 
@@ -11,35 +12,47 @@ learning_rate = 0.1
 discount_factor = 0.99
 epsilon = 1.0
 epsilon_min = 0.01
-epsilon_decay = 0.995
-n_episodes = 1000
-max_steps = 500
+epsilon_decay = 0.9995
+n_episodes = 5000
+max_steps = 1000
 
-Q = np.zeros((n_states, n_actions))
+# Define o número de intervalos para cada valor contínuo
+num_bins = [10, 10, 10, 10]
 
+# Define os limites de cada intervalo para cada valor contínuo
+bin_limits = [(-1.0, 1.0), (-2.0, 2.0), (-3.0, 3.0), (-4.0, 4.0)]
 
-# Definir os limites para cada componente do estado
-pos_bins = np.linspace(-4.8, 4.8, 9)
-vel_bins = np.linspace(-2.0, 2.0, 9)
-ang_bins = np.linspace(-0.5, 0.5, 9)
-ang_vel_bins = np.linspace(-3.5, 3.5, 9)
+# Define a forma da tabela Q-values
+num_actions = 2
+q_shape = tuple(num_bins + [num_actions])
+
+# Cria uma tabela Q-values zerada
+q_table = np.zeros(q_shape)
+
 
 def discretize_state(state):
+    # Discretiza cada valor contínuo usando intervalos fixos
+    discretized_state = [np.digitize([s], np.linspace(l, u, n + 1))[0] for s, (l, u), n in
+                         zip(state, bin_limits, num_bins)]
 
-    pos_bin = np.digitize(state[0][0], pos_bins)
-    vel_bin = np.digitize(state[0][1], vel_bins)
-    ang_bin = np.digitize(state[0][2], ang_bins)
-    ang_vel_bin = np.digitize(state[0][3], ang_vel_bins)
-    return pos_bin, vel_bin, ang_bin, ang_vel_bin
+    # Verifica se os valores discretizados estão dentro dos limites
+    discretized_state = [min(max(0, ds), nb - 1) for ds, nb in zip(discretized_state, num_bins)]
+
+    return discretized_state
 
 
 # Loop sobre o número de episódios
 for episode in range(n_episodes):
 
-    state = env.reset()
+    state = env.reset()[0]
 
     done = False
     t = 0
+
+    total_reward = 0
+
+    ran = 0
+    tab = 0
 
     # Reduzir o valor de epsilon para cada episódio
     if epsilon > epsilon_min:
@@ -47,11 +60,16 @@ for episode in range(n_episodes):
 
     # Loop sobre o número máximo de etapas por episódio
     while not done and t < max_steps:
+
+        state_discrete = discretize_state(state)
+
         # Escolher uma ação usando epsilon-greedy
         if np.random.random() < epsilon:
             action = env.action_space.sample()
+            ran +=1
         else:
-            action = np.argmax(Q[state, :])
+            action = np.argmax(q_table[tuple(state_discrete)])
+            tab +=1
 
         # Realizar a ação escolhida e receber a próxima observação, a recompensa e um indicador se o episódio terminou
         observation  = env.step(action)
@@ -60,17 +78,36 @@ for episode in range(n_episodes):
         reward = observation[1]
 
         # Atualizar a tabela Q
-        state_discrete = discretize_state(state)
+
         next_state_discrete = discretize_state(next_state)
-        Q[state_discrete][action] += learning_rate * (reward + discount_factor * np.max(Q[next_state_discrete]) - Q[state_discrete][action])
+
+
+        # Calcular o valor Q antigo e o novo valor Q
+        old_value = q_table[tuple(state_discrete) + (action,)]
+
+        try:
+            next_max = np.max(q_table[tuple(next_state_discrete) + (action,)])
+        except:
+            print("erro")
+
+        new_value = old_value + learning_rate * (reward + discount_factor * next_max - old_value)
+
+        q_table[tuple(state_discrete) + (action,)] += learning_rate * (reward + discount_factor * np.max(q_table[tuple(next_state_discrete) + (action,)]) - q_table[tuple(state_discrete) + (action,)])
 
 
         # Atualizar o estado atual
         state = next_state
         t += 1
 
+        total_reward += reward
+
+
     # Imprimir a recompensa acumulada do episódio
-    print("Episódio {}: Recompensa acumulada = {}".format(episode, t))
+    print("Episódio {}: Recompensa acumulada: {} Epsilon: {} random: {} q_table {}".format(episode, total_reward, epsilon, ran, tab))
 
 # Fechar o ambiente
 env.close()
+
+fw = open('cartpolev1_agent', 'wb')
+pickle.dump(q_table, fw)
+fw.close()
--- cart_pole/cart_pole_human.py
+++ cart_pole/cart_pole_human.py
@@ -1,19 +1,44 @@
 import gym
 import numpy as np
 import time
+import pickle
 
 env = gym.make('CartPole-v1', render_mode='human')
 
-(state,_) = env.reset()
+state = env.reset()[0]
 
-x = state[0]
-x_dot = state[1]
-theta = state[2]
-theta_dot = state[3]
+fr = open('cartpolev1_agent', 'rb')
+q_table = pickle.load(fr)
+fr.close()
 
-obs = env.observation_space
+# Define o número de intervalos para cada valor contínuo
+num_bins = [10, 10, 10, 10]
+
+# Define os limites de cada intervalo para cada valor contínuo
+bin_limits = [(-1.0, 1.0), (-2.0, 2.0), (-3.0, 3.0), (-4.0, 4.0)]
+
+
+def discretize_state(state):
+    # Discretiza cada valor contínuo usando intervalos fixos
+    discretized_state = [np.digitize([s], np.linspace(l, u, n + 1))[0] for s, (l, u), n in
+                         zip(state, bin_limits, num_bins)]
+
+    # Verifica se os valores discretizados estão dentro dos limites
+    discretized_state = [min(max(0, ds), nb - 1) for ds, nb in zip(discretized_state, num_bins)]
+
+    return discretized_state
+
+action = 0
 
 while True:
-    time.sleep(0.2)
-    env.render()
-    env.step(0)
+
+    state = discretize_state(state)
+    time.sleep(0.1)
+    action = np.argmax(q_table[tuple(state)])
+    # Realizar a ação escolhida e receber a próxima observação, a recompensa e um indicador se o episódio terminou
+    observation = env.step(action)
+
+    state = observation[0]
+
+env.render()
+env.step(0)
